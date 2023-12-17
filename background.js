278c9f4adf6134c06947a3b88bb727f290f1fa9d
Nico Vignola <nicolantonio.vignola@hotjar.com>
2023-12-17 16:52:36 +0100

    Migrate to V3 manifest

--- background.js
+++ background.js
@@ -9,18 +9,16 @@ chrome.tabs.onCreated.addListener(updateBadge);
 
 function updateBadge() {
   // get active tab on current window
-  chrome.tabs.query({ active: true, currentWindow: true }, function(
-    arrayOfTabs
-  ) {
+  chrome.tabs.query({ active: true, currentWindow: true }, (arrayOfTabs) => {
     const [activeTab] = arrayOfTabs;
 
     if (!activeTab) return;
 
-    const parsedQueryString = urlInfo(activeTab.url).parsedQueryString;
+    const { parsedQueryString } = urlInfo(activeTab.url);
     // compute number for badge for current tab's url
-    chrome.browserAction.setBadgeText({
+    chrome.action.setBadgeText({
       text: parsedQueryString.length.toString(),
-      tabId: activeTab.id
+      tabId: activeTab.id,
     });
   });
 }
